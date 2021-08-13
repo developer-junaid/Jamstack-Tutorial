39bdd16891cb4527c4d676f5d0e79f3d1c8837c4
developer-junaid <qjunaid623@gmail.com>
2021-08-13 09:05:09 +0500

    Added Form Validation with formik, added Saving Loader

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -1,75 +1,131 @@
-import React, { useState } from "react";
+import React from "react";
 
+// Formik
+import { Formik } from "formik";
+
+// Component
 const LinkForm = ({ refreshLinks }) => {
-  const [name, setName] = useState("");
-  const [url, setUrl] = useState("");
-  const [description, setDescription] = useState("");
-
-  // Handlers
-  const resetForm = () => {
-    setName("");
-    setUrl("");
-    setDescription("");
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // data
-    const body = { name, url, description };
-
-    try {
-      const res = await fetch("/.netlify/functions/createLink", {
-        method: "POST",
-        body: JSON.stringify(body),
-      });
-
-      resetForm();
-      refreshLinks();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <div className="card">
       <div className="card-header">Add Link</div>
       <div className="card-body">
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              name="name"
-              className="form-control"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="url">URL</label>
-            <input
-              type="text"
-              name="url"
-              className="form-control"
-              value={url}
-              onChange={(e) => setUrl(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="description">Description</label>
-            <input
-              type="text"
-              name="description"
-              className="form-control"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-          </div>
+        <Formik
+          // Initial Values
+          initialValues={{
+            name: "",
+            url: "",
+            description: "",
+          }}
+          // Validate
+          validate={(values) => {
+            const errors = {};
 
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
+            if (!values.name) {
+              errors.name = "Required";
+            }
+            if (!values.url) {
+              errors.url = "Required";
+            }
+            if (!values.description) {
+              errors.description = "Required";
+            }
+
+            // If errors
+            return errors;
+          }}
+          // On Submit
+          onSubmit={async (values, { resetForm }) => {
+            // logic
+            // data
+            const body = {
+              name: values.name,
+              url: values.url,
+              description: values.description,
+            };
+
+            try {
+              await fetch("/.netlify/functions/createLink", {
+                method: "POST",
+                body: JSON.stringify(body),
+              });
+            } catch (error) {
+              console.log(error);
+            }
+
+            resetForm();
+            refreshLinks();
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <div className="form-group pb-2">
+                <label htmlFor="name">Name*</label>
+                <input
+                  type="text"
+                  name="name"
+                  className="form-control"
+                  value={values.name}
+                  onChange={handleChange}
+                />
+                <span className="text-danger">
+                  {errors.name && touched.name && errors.name}
+                </span>
+              </div>
+              <div className="form-group pb-2">
+                <label htmlFor="url">URL*</label>
+                <input
+                  type="text"
+                  name="url"
+                  className="form-control"
+                  value={values.url}
+                  onChange={handleChange}
+                />
+                <span className="text-danger">
+                  {errors.url && touched.url && errors.url}
+                </span>
+              </div>
+              <div className="form-group pb-2">
+                <label htmlFor="description">Description*</label>
+                <input
+                  type="text"
+                  name="description"
+                  className="form-control"
+                  value={values.description}
+                  onChange={handleChange}
+                />
+                <span className="text-danger">
+                  {errors.description &&
+                    touched.description &&
+                    errors.description}
+                </span>
+              </div>
+
+              {isSubmitting ? (
+                <>
+                  <button className="btn btn-primary" type="submit" disabled>
+                    <span
+                      className="spinner-border spinner-border-sm"
+                      role="status"
+                      aria-hidden="true"
+                    ></span>
+                    <span className="ml-4"> Saving...</span>
+                  </button>
+                </>
+              ) : (
+                <button type="submit" className="btn btn-primary">
+                  Save
+                </button>
+              )}
+            </form>
+          )}
+        </Formik>
       </div>
     </div>
   );
